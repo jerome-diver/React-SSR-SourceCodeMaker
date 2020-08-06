5b6d2e4ef5a75f44b920e80d08cf77cb291ac305
Jérôme <jerome.archlinux@gmail.com>
2020-08-06 17:55:35 +0200

    add loading automation message to home frontend component (and a specific state for this)

--- frontend/routes/Home.component.js
+++ frontend/routes/Home.component.js
@@ -3,29 +3,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Home = (props) => {
-  const [home, setHome] = useState({
-    title: "",
-    content: "" 
-  })
+  const [home, setHome] = useState({ title: "", content: "" })
+  const [load, setLoad] = useState(false)
 
   useEffect( () => {
       console.log("UseEffect of Home component call")
       fetch('http://localhost:3000/api/home')
         .then(res => res.json())
-        .then(respond => setHome( 
-            { title: respond.title,
-              content: respond.content } ) )
+        .then(respond => {
+          setHome( { title: respond.title, content: respond.content } )
+          setLoad(true) } )
   }, [] )
 
-  return (
-    <>
-        <h1><FontAwesomeIcon icon={faCoffee} size="xs" color="blue" /> {home.title}</h1>
-        <hr />
-        <div id="home_article">
-          <p>{ home.content }</p>
-        </div>
-    </>
-  )
+  if (load) {
+    return (
+      <>
+          <h1><FontAwesomeIcon icon={faCoffee} size="xs" color="blue" /> {home.title}</h1>
+          <hr />
+          <div id="home_article">
+            <p>{ home.content }</p>
+          </div>
+      </>
+    )
+} else {
+  return (<div>Loading...</div>)
+}
 }
 
 export default Home;
