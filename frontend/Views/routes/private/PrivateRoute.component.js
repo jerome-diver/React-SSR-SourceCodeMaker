1cc58ade3c973e17349f29c38ddabbfea5d8a214
Jérôme <jerome.archlinux@gmail.com>
2020-08-17 13:48:58 +0200

    first step user authentication, create Context

--- frontend/Views/routes/private/PrivateRoute.component.js
+++ frontend/Views/routes/private/PrivateRoute.component.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import { isAuthenticated } from '../../../Controllers/user/authenticate-helper'
+//import { isAuthenticated } from '../../../Controllers/user/authenticate-helper'
+import { useAuth } from '../../../Controllers/context/authenticate'
 
-const  PrivateRoute = (props) => {
-    <Route path={props.path} component={ (props) => isAuthenticated ? ( <Component {...props} /> ) : 
-                            ( <Redirect to={{ pathname: '/signin', state: { from: props.location } }} /> ) } />
+const  PrivateRoute = ({component: Component, ...rest}) => {
+
+const isAuthenticated = useAuth()
+
+    return (
+        <Route {...rest} render={ 
+        (props) => isAuthenticated 
+            ? ( <Component {...props} /> ) 
+            : ( <Redirect to={{ pathname: '/signin', state: { from: props.location } }} /> ) 
+    } /> )
 }
 
 export default PrivateRoute
