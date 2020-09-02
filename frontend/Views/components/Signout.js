55c36594ee680bebf134a3fb4bd65802f4db99c5
Jérôme <jerome.archlinux@gmail.com>
2020-09-02 11:13:59 +0200

    cleaning debug SignOut component

--- frontend/Views/components/Signout.js
+++ frontend/Views/components/Signout.js
@@ -5,7 +5,6 @@ import { signout } from '../../Controllers/user/authenticate-api'
 import { useAuthenticate } from '../../Controllers/context/authenticate'
 
 const reducer = (state, action) => {
-    console.log("--- REDUCER --- with loaded:", action.loaded)
     return { loaded: action.loaded,
              error: action.error,
              loggedOut: action.loggedOut }
@@ -18,23 +17,18 @@ const SignOut = (props) => {
     const [ state, dispatch ] = useReducer(reducer, { loggedOut: false, loaded: false })
 
     useEffect( () => {
-        console.log("--- useEffect from SignOut --- state:", state)
-        const abort = new AbortController()     // stop to fetch a request if we cancel this page
+        const abort = new AbortController()
         const signal = abort.signal
         logOut(getUser(), signal)
-        
         return function cleanup() { abort.abort() }
     }, [])
 
     const logOut = (user, signal) => {
-        console.log("--- SignOut logOut --- user & state are: ", user, state)
         if (user) {
             signout(user.id, signal).then(result => {
                 if (result.error) dispatch({error: result.error, loggedOut: false, loaded: true}) 
-                else if (result == true) { 
-                    console.log("--- Signout user signed out right now --- state is:", state)
-                    setUserSession(0)
-                } else { dispatch({loaded: true, loggedOut: false, error: 'LogOut rejected'}) } } )
+                else if (result == true) setUserSession(0)
+                else dispatch({loaded: true, loggedOut: false, error: 'LogOut rejected'}) } )
         } else { dispatch({loaded: true, loggedOut: true, error: ''}) }
     }
     const closeModal = () => { setError('') }
