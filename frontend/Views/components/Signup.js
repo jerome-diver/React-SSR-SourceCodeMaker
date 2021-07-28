e5938bedef955253c1ca090da54c4fec46864e19
Jérôme <jerome.archlinux@gmail.com>
2021-07-28 12:23:25 +0700

    prepare that ezventualy, i can stop to use javascript popup for a modal popup... to use my Error Comùponent instead.

--- frontend/Views/components/Signup.js
+++ frontend/Views/components/Signup.js
@@ -8,19 +8,19 @@ import { useTranslation } from 'react-i18next'
 import { fireError, validatePassword, sendEmailLink } from '../../Controllers/user/user-form-helper'
 import { create } from '../../Controllers/user/action-CRUD'
 import { useCookies } from 'react-cookie'
-import { Loading } from '../Pages/public/Printers.component'
+import { Loading, Error } from '../Pages/public/Printers.component'
 
 const SignUp = (props) => {
     const { i18n, t } = useTranslation()
     const [user, setUser] = useState({ username: "", email: '', pass1: '', pass2: ''})
-    const [load, setLoad] = useState(false)
+    const [loaded, setLoaded] = useState(false)
     const [submit, setSubmit] = useState(false)
     const [redirect, setRedirect] = useState('signup')
     const location = useLocation()
     const [ cookies, setCookies, removeCookies ] = useCookies(['session'])
     
     useEffect( () => {
-        setLoad(!submit)
+        setLoaded(!submit)
     }, [submit, t] )
     
     const handleChange = name => event => { setUser({...user, [name]: event.target.value}) }
@@ -50,55 +50,54 @@ const SignUp = (props) => {
         if (cookies.session && cookies.session.user) { return <Redirect to={'/'}/> }
     }
 
-    if (load) {
-        return (
-            <Card id='sign'>
-            {renderRedirect()}
-                <Card.Header><h2><FontAwesomeIcon icon={faUserPlus} /> {t('signup.title')}</h2></Card.Header>
-                <Card.Body>
-                    <Card.Title>{t('signup.header.title')}</Card.Title>
-                    <Card.Subtitle className='mb-2 text-muted'>{t('signup.header.intro')}</Card.Subtitle> 
-                    <Card.Text>{t('signup.header.description')}</Card.Text>
-                    <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>{t('signup.email.label')} </Form.Label>
-                        <Form.Control type='email' placeholder={t('signup.email.placeholder')}
-                                      defaultValue={user.email}
-                                      onChange={handleChange('email')} />
-                        <Form.Text className='text-muted'>{t('signup.email.helper')}</Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicText">
-                        <Form.Label>{t('signup.username.label')}</Form.Label>
-                        <Form.Control type='text' placeholder={t('signup.username.placeholder')} 
-                                      defaultValue={user.username}
-                                      onChange={handleChange('username')} />
-                        <Form.Text className="text-muted">{t('signup.username.helper')}</Form.Text>
-                    </Form.Group>
+    if (!loaded) return <><Loading /></>
+    return (
+        <Card id='sign'>
+        {renderRedirect()}
+            <Card.Header><h2><FontAwesomeIcon icon={faUserPlus} /> {t('signup.title')}</h2></Card.Header>
+            <Card.Body>
+                <Card.Title>{t('signup.header.title')}</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>{t('signup.header.intro')}</Card.Subtitle> 
+                <Card.Text>{t('signup.header.description')}</Card.Text>
+                <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>{t('signup.email.label')} </Form.Label>
+                    <Form.Control type='email' placeholder={t('signup.email.placeholder')}
+                                    defaultValue={user.email}
+                                    onChange={handleChange('email')} />
+                    <Form.Text className='text-muted'>{t('signup.email.helper')}</Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicText">
+                    <Form.Label>{t('signup.username.label')}</Form.Label>
+                    <Form.Control type='text' placeholder={t('signup.username.placeholder')} 
+                                    defaultValue={user.username}
+                                    onChange={handleChange('username')} />
+                    <Form.Text className="text-muted">{t('signup.username.helper')}</Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>{t('signup.password.label')}</Form.Label>
+                    <Form.Control type='password' placeholder={t('signup.password.placeholder')} 
+                                    onChange={handleChange('pass1')}/>
+                    <Form.Text className='text-muted'>{t('signup.password.helper')}</Form.Text>
+                </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>{t('signup.password.label')}</Form.Label>
-                        <Form.Control type='password' placeholder={t('signup.password.placeholder')} 
-                                      onChange={handleChange('pass1')}/>
-                        <Form.Text className='text-muted'>{t('signup.password.helper')}</Form.Text>
+                        <Form.Label>{t('signup.password_repeat.label')} </Form.Label>
+                        <Form.Control type='password' placeholder={t('signup.password_repeat.placeholder')} 
+                                        onChange={handleChange('pass2')} />
+                        <Form.Text className='text-muted'>{t('signup.password_repeat.helper')}</Form.Text>
                     </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>{t('signup.password_repeat.label')} </Form.Label>
-                            <Form.Control type='password' placeholder={t('signup.password_repeat.placeholder')} 
-                                          onChange={handleChange('pass2')} />
-                            <Form.Text className='text-muted'>{t('signup.password_repeat.helper')}</Form.Text>
-                        </Form.Group>
-                    </Form>
-                </Card.Body>
-                <Card.Footer>
-                    <Card.Link>
-                        <Button type='submit' onClick={clickSubmit}>
-                            <FontAwesomeIcon icon={faUserPlus} /> {t('signup.button_submit')}
-                        </Button>
-                    </Card.Link>
-                    <Card.Link href='/signin'>{t('signup.link.have_account')}</Card.Link>
-                </Card.Footer>
-            </Card>
-        )
-    } else return <><Loading /></>
+                </Form>
+            </Card.Body>
+            <Card.Footer>
+                <Card.Link>
+                    <Button type='submit' onClick={clickSubmit}>
+                        <FontAwesomeIcon icon={faUserPlus} /> {t('signup.button_submit')}
+                    </Button>
+                </Card.Link>
+                <Card.Link href='/signin'>{t('signup.link.have_account')}</Card.Link>
+            </Card.Footer>
+        </Card>
+    )
 }
 
 export default SignUp
